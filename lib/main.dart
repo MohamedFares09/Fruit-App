59975c0623595176fbefdb756f54c698aca6c5a9
MohamedFares09 <mofares010@gmail.com>
2025-08-31 03:33:24 +0300

    add firebase in projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_app/core/hleper_functions/on_generate_route.dart';
 import 'package:fruits_app/core/services/shared_preferences_single_tone.dart';
 import 'package:fruits_app/features/splash/presentation/view/splash_view.dart';
+import 'package:fruits_app/firebase_options.dart';
 import 'package:fruits_app/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferencesService.init();
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FruitApp());
 }
 
