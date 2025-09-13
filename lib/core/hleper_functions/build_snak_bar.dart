eacd76945e6d9f4f4265e29a7f4743f3b9ef4725
MohamedFares09 <mofares010@gmail.com>
2025-09-13 15:27:27 +0300

    fix

--- lib/core/hleper_functions/build_snak_bar.dart
+++ lib/core/hleper_functions/build_snak_bar.dart
@@ -1,5 +1,4 @@
   import 'package:flutter/material.dart';
-import 'package:fruits_app/features/auth/presentation/cubits/signup_cubit.dart/signup_state.dart';
 
 void buildsnakbar(BuildContext context,  String message) {
     ScaffoldMessenger.of(context).showSnackBar(
--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_app/core/hleper_functions/get_it_function.dart';
 import 'package:fruits_app/core/utils/widgets/custom_app_bar.dart';
-import 'package:fruits_app/core/utils/widgets/custom_progress_hud.dart';
 import 'package:fruits_app/features/auth/doman/repos/auth_repo.dart';
 import 'package:fruits_app/features/auth/presentation/cubits/signin/signin_cubit.dart';
-import 'package:fruits_app/features/auth/presentation/widgets/signin_view_body.dart';
 import 'package:fruits_app/features/auth/presentation/widgets/signin_view_body_bloc_consumer.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
